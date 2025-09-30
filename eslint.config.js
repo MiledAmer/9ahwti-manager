d9b1495a8c7ef94579a8dd6e6ccf9a2a5981f9e8
MiledAmer <midou.slm8@gmail.com>
2025-09-30 10:49:53 +0100

    add barchart from tremor

--- eslint.config.js
+++ eslint.config.js
@@ -7,7 +7,7 @@ const compat = new FlatCompat({
 
 export default tseslint.config(
   {
-    ignores: [".next"],
+    ignores: [".next", "src/components/tremot", "src/lib/chartUtils.ts"],
   },
   ...compat.extends("next/core-web-vitals"),
   {
--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { AppSidebar } from "@/modules/dashboard/components/app-sidebar";
 import {
   Breadcrumb,
@@ -13,6 +14,86 @@ import {
   SidebarProvider,
   SidebarTrigger,
 } from "@/components/ui/sidebar";
+
+import { BarChart } from "@/components//tremot/bar-chart";
+
+const chartdata = [
+  {
+    date: "Jan 23",
+    SolarPanels: 2890,
+    Inverters: 2338,
+  },
+  {
+    date: "Feb 23",
+    SolarPanels: 2756,
+    Inverters: 2103,
+  },
+  {
+    date: "Mar 23",
+    SolarPanels: 3322,
+    Inverters: 2194,
+  },
+  {
+    date: "Apr 23",
+    SolarPanels: 3470,
+    Inverters: 2108,
+  },
+  {
+    date: "May 23",
+    SolarPanels: 3475,
+    Inverters: 1812,
+  },
+  {
+    date: "Jun 23",
+    SolarPanels: 3129,
+    Inverters: 1726,
+  },
+  {
+    date: "Jul 23",
+    SolarPanels: 3490,
+    Inverters: 1982,
+  },
+  {
+    date: "Aug 23",
+    SolarPanels: 2903,
+    Inverters: 2012,
+  },
+  {
+    date: "Sep 23",
+    SolarPanels: 2643,
+    Inverters: 2342,
+  },
+  {
+    date: "Oct 23",
+    SolarPanels: 2837,
+    Inverters: 2473,
+  },
+  {
+    date: "Nov 23",
+    SolarPanels: 2954,
+    Inverters: 3848,
+  },
+  {
+    date: "Dec 23",
+    SolarPanels: 3239,
+    Inverters: 3736,
+  },
+];
+
+const BarChartAxisLabelsExample = () => (
+  <BarChart
+    className="h-full px-2"
+    data={chartdata}
+    index="date"
+    categories={["SolarPanels", "Inverters"]}
+    valueFormatter={(number: number) =>
+      `$${Intl.NumberFormat("us").format(number).toString()}`
+    }
+    onValueChange={(v) => console.log(v)}
+    xAxisLabel="Month"
+    yAxisLabel="Spend Category"
+  />
+);
 
 export default function Page() {
   return (
@@ -47,7 +128,9 @@ export default function Page() {
             <div className="bg-muted/50 aspect-video rounded-xl" />
             <div className="bg-muted/50 aspect-video rounded-xl" />
           </div>
-          <div className="bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min" />
+          <div className="bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min">
+            <BarChartAxisLabelsExample />
+          </div>
         </div>
       </SidebarInset>
     </SidebarProvider>
